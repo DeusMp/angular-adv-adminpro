96e935662973d3290ce8ec0f1e94a83b339f5bc3
Amado Ibarra <deus.mpa@gmail.com>
2020-08-22 00:38:46 -0500

    fin seccion 3 - diseño

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls: [
+    './nopagefound.component.css'
+  ]
+})
+export class NopagefoundComponent {
+
+  constructor() { }
+
+  year = new Date().getFullYear();
+}
---
+++ src/app/shared/right-sidebar/right-sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-right-sidebar',
+  templateUrl: './right-sidebar.component.html',
+  styles: [
+  ]
+})
+export class RightSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
